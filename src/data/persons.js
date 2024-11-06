70ea10346ed6b32eb5127f704962274e94eba8eb
Nikita Luzianin <s2400796@edu.bc.fi>
2024-11-06 11:07:20 +0200

    add persons

---
+++ src/data/persons.js
@@ -0,0 +1,84 @@
+const personsData = [
+    {
+        id: 0,
+        name: "name 1",
+        role: "role 1",
+        department: "department 1",
+        startDate: "2024-06-01",
+        location: "location1"
+    },
+    {
+        id: 1,
+        name: "name 2",
+        role: "role 1",
+        department: "department 1",
+        startDate: "06.2024",
+        location: "location2"
+    },
+    {
+        id: 2,
+        name: "name 3",
+        role: "role 1",
+        department: "department 1",
+        startDate: "06.2024",
+        location: "location3"
+    },
+    {
+        id: 3,
+        name: "name 4",
+        role: "role 1",
+        department: "department 2",
+        startDate: "06.2024",
+        location: "location4"
+    },
+    {
+        id: 4,
+        name: "name 5",
+        role: "role 1",
+        department: "department 2",
+        startDate: "06.2024",
+        location: "location1"
+    },
+    {
+        id: 5,
+        name: "name 6",
+        role: "role 1",
+        department: "department 2",
+        startDate: "06.2024",
+        location: "location2"
+    },
+    {
+        id: 6,
+        name: "name 7",
+        role: "role 1",
+        department: "department 3",
+        startDate: "06.2024",
+        location: "location3"
+    },
+    {
+        id: 7,
+        name: "name 8",
+        role: "role 1",
+        department: "department 3",
+        startDate: "06.2024",
+        location: "location4"
+    },
+    {
+        id: 8,
+        name: "name 9",
+        role: "role 1",
+        department: "department 3",
+        startDate: "06.2024",
+        location: "location1"
+    },
+    {
+        id: 9,
+        name: "name 10",
+        role: "role 1",
+        department: "department 3",
+        startDate: "06.2024",
+        location: "location2"
+    },
+];
+
+export default personsData;
